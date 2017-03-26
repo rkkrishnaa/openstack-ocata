8ee04a5db9511a43533009dce66361ad99443efa
rk <radhakrishnan.r@cloudenablers.com>
2017-03-26 18:33:10 +0530

    changed installer file

--- installer.sh
+++ installer.sh
@@ -2,3 +2,6 @@ source userinput.sh
 bash install-basic-services.sh 
 bash install-keystone-service.sh
 bash install-glance-service.sh
+bash install-nova-service.sh
+bash install-neutron-service.sh
+bash install-horizon.sh
